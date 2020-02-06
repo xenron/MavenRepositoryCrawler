d6e2f138361dbab26022320ef5d1774e44dd01b6
wy1986 <geniuswy123@126.com>
2020-02-06 18:14:18 +0800

    update

--- biz/mvncompile.py
+++ biz/mvncompile.py
@@ -1,14 +1,18 @@
 import os
 import xml.etree.ElementTree as ET
 from utils.log import getLogger
+from utils.common import ConfigUtil
 
+# 读取配置文件
+config = ConfigUtil()
 
 def rewrite_xml(item):
     logger = getLogger()
     logger.info("method [rewrite_xml] start")
 
-    color_xml = "D:\Work\WangYue\\testmaven\pom.xml"
-    updateTree = ET.parse(color_xml)
+    pwd = os.getcwd()
+    pom_path = os.path.join(pwd, "data", "pom.xml")
+    updateTree = ET.parse(pom_path)
     XML_NS_NAME = ""
     XML_NS_VALUE = "http://maven.apache.org/POM/4.0.0"
     ET.register_namespace(XML_NS_NAME, XML_NS_VALUE)
@@ -25,16 +29,24 @@ def rewrite_xml(item):
         elif de.tag == pre + 'version':
             de.text = item.version
 
-    updateTree.write(color_xml)
+    updateTree.write(pom_path)
     logger.info("method [rewrite_xml] end")
 
 
 def mvn_compile():
     logger = getLogger()
     logger.info("method [mvn_compile] start")
+    mvn_path = config.load_value('system', 'mvn_path', '')
 
-    os.chdir(r"D:\Work\WangYue\testmaven")
-    a = os.system(r'D:\soft\Develop\apache-maven-3.5.0\bin\mvn compile 2>out.txt')
+    pwd = os.getcwd()
+    pom_path = os.path.join(pwd, "data", "pom.xml")
+    log_path = os.path.join(pwd, "logs", "error.txt")
+    command = "{0} -f {1} compile 2>{2}".format(mvn_path, pom_path, log_path)
+    a = os.system(command)
 
     logger.info("method [mvn_compile] end")
     return a
+
+
+if __name__ == '__main__':
+    mvn_compile()
